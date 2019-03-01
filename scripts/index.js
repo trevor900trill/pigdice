eb8d6cf9d7178c18f969f6bbacb2cddb34dab87c
Trevor Mugo <trevormugolawrence@gmail.com>
2019-03-01 16:18:56 +0300

    startpagedone

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,11 @@
 //BUSINESS LOGIC
 console.log("hello world");
-
 //USER INTERFACE
 $(document).ready(function(){
-
+  $(".beginnerbtn").on("click",function(){
+    sessionStorage.setItem("level","easy");
+  });
+  $(".probtn").on("click",function(){
+    sessionStorage.setItem("level","hard");
+  });
 });
