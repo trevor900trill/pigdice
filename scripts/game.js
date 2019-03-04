b45046f267099b0b85639b7076955c5cb4320c3c
Trevor Mugo <trevormugolawrence@gmail.com>
2019-03-04 08:22:58 +0300

    logic3

--- scripts/game.js
+++ scripts/game.js
@@ -1,29 +1,53 @@
 //BUSIINESS LOGIC
-//Easy logic
+//easy LOGIC
+//rolls only once then stops
+var choice = ["stop","continue"];
+var easyroll = () => {
+  //guess a number btn 1 - 6
+  var num = Math.floor(Math.random()*6);
+  if (num === 1 )
+  {
+    //user scores nothing anf=d has to hold;
+    alert("no reroll");
+  }
+  alert(num);
+};
 
-//USER INTERFACE LOGIC
-$(document).ready(function(){
+//click handler
+var gamemode = () => {
   var checkLvl = sessionStorage.getItem("level");
   if(checkLvl === "easy")
   {
-    //the functionality for easy goes here;
-    var name = prompt("What is you're name?");
-    document.getElementsByClassName("playername")[0].textContent = name;
-    document.getElementsByClassName("levelof")[0].textContent = "  LEVEL : EASY";
-
-  }
-  else if(checkLvl === "hard")
-  {
-    //the functionality for hard goes here
-    var name = prompt("What is you're name?");
-    document.getElementsByClassName("playername")[0].textContent = name;
-    document.getElementsByClassName("levelof")[0].textContent = "  LEVEL : DIFFICULT";
+    easyroll();
   }
   else
   {
-    //an error occured
-    var name = prompt("What is you're name?");
-    document.getElementsByClassName("playername")[0].textContent = name;
-    document.getElementsByClassName("levelof")[0].textContent = "ERROR";
+
   }
+};
+//USER INTERFACE LOGIC
+$(document).ready(function(){
+    var checkLvl = sessionStorage.getItem("level");
+    if(checkLvl === "easy")
+    {
+      //the functionality for easy goes here;
+      var name = prompt("What is you're name?");
+      document.getElementsByClassName("playername")[0].textContent = name;
+      document.getElementsByClassName("levelof")[0].textContent = "  LEVEL : EASY";
+
+    }
+    else if(checkLvl === "hard")
+    {
+      //the functionality for hard goes here
+      var name = prompt("What is you're name?");
+      document.getElementsByClassName("playername")[0].textContent = name;
+      document.getElementsByClassName("levelof")[0].textContent = "  LEVEL : DIFFICULT";
+    }
+    else
+    {
+      //an error occured
+      var name = prompt("What is you're name?");
+      document.getElementsByClassName("playername")[0].textContent = name;
+      document.getElementsByClassName("levelof")[0].textContent = "ERROR";
+    }
 });
