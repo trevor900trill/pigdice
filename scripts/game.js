ef619681b570fa6641bfa41c01836aefd09ba3f1
Trevor Mugo <trevormugolawrence@gmail.com>
2019-03-04 15:00:39 +0300

    logic5

--- scripts/game.js
+++ scripts/game.js
@@ -1,27 +1,48 @@
 //BUSIINESS LOGIC
 //easy LOGIC
 //rolls only once then stops
-var userscore;
-var comproll = () => {
+var userscorelist = [];
+var easycomputerroll = [];
+var easycomproll = () => {
+  var num = Math.floor(Math.random()*6)+1;
 
 };
 var playereasyroll = (callback) => {
   //guess a number btn 1 - 6
-  var num = Math.floor(Math.random()*6);
+  var num = Math.floor(Math.random()*6)+1;
   if (num === 1 )
   {
+    alert(num);
+    addresult();
+    easycomproll();
     //user scores nothing anf=d has to hold;
     //function for computer to roll dice
-    alert("no reroll");
+    document.getElementsByClassName('errorstatuscompon')[0].textContent = "you rolled a one no reroll you have to hold";
+    document.getElementsByClassName('successstatuscompon')[0].textContent = "";
     //hide the button to prevent further clicks
     $(".buttonbox").hide();
+    //userscores nothing dont push to array;
   }
   else
   {
     //continue;
     alert(num);
+    userscorelist.push(num);
+    var render = document.getElementById("live");
+    render.innerHTML += "<li>"+num+"</li>";
   }
 };
+
+
+
+var addresult = () => {
+  var result = eval(userscorelist.join('+'));
+  alert(result);
+  document.getElementsByClassName("actuallcount")[0].textContent = result;
+};
+
+
+
 //click handler
 var gamemode = () => {
   var checkLvl = sessionStorage.getItem("level");
