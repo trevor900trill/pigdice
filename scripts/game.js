d7248015b12c29f6b254657451aab4cee48a1828
Trevor Mugo <trevormugolawrence@gmail.com>
2019-03-04 15:49:23 +0300

    logic7

--- scripts/game.js
+++ scripts/game.js
@@ -30,7 +30,7 @@ var easycomproll = () => {
   }
 };
 
-var playereasyroll = (callback) => {
+var playereasyroll = () => {
   //guess a number btn 1 - 6
   var num = Math.floor(Math.random()*6)+1;
   if (num === 1 )
@@ -60,15 +60,70 @@ var playereasyroll = (callback) => {
 
 var addresultcomp = () => {
   var result = eval(easycomputerroll.join('+'));
-  alert(result);
-  document.getElementsByClassName("npcactuallcount")[0].textContent = result;
+  if(result > 100 || result === 100)
+  {
+    alert("100 player 2 won");
+    $(".buttonbox").hide();
+    location.reload();
+  }
+  else if(result === undefined)
+  {
+    alert("player 2 rolled a one");
+  }
+  else
+  {
+    alert("player 2 new total score " +result);
+    document.getElementsByClassName("npcactuallcount")[0].textContent = result;
+  }
 }
 var addresultuser = () => {
   var result = eval(userscorelist.join('+'));
-  alert(result);
-  document.getElementsByClassName("actuallcount")[0].textContent = result;
+  if(result > 100 || result === 100)
+  {
+    alert("100 player 1 won");
+    document.getElementsByClassName("actuallcount")[0].textContent = "100";
+    $(".buttonbox").hide();
+    location.reload();
+  }
+  else if(result === undefined)
+  {
+    alert("you rolled a one");
+  }
+  else
+  {
+    document.getElementsByClassName("actuallcount")[0].textContent = result;
+
+  }
 };
 
+
+var playerdifficultroll = () => {
+  //guess a number btn 1 - 6
+  var num = Math.floor(Math.random()*6)+1;
+  if (num === 1 )
+  {
+    alert(num);
+    addresultuser();
+    //user scores nothing anf=d has to hold;
+    //function for computer to roll dice
+    document.getElementsByClassName('errorstatuscompon')[0].textContent = "you rolled a one you have to hold";
+    document.getElementsByClassName('successstatuscompon')[0].textContent = "";
+    //hide the button to prevent further clicks
+    $(".buttonbox").hide();
+    //userscores nothing dont push to array;
+    setTimeout(function(){
+      easycomproll();
+    },2000);
+  }
+  else
+  {
+    //continue;
+    alert(num);
+    userscorelist.push(num);
+    var render = document.getElementById("live");
+    render.innerHTML += "<li>"+num+"</li>";
+  }
+}
 
 
 //click handler
@@ -97,7 +152,7 @@ $(document).ready(function(){
     if(checkLvl === "easy")
     {
       //the functionality for easy goes here;
-      var name = prompt("What is you're name?");
+      var name = prompt("NEW  GAME:What is you're name?");
       document.getElementsByClassName("playername")[0].textContent = name;
       document.getElementsByClassName("levelof")[0].textContent = "  LEVEL : EASY";
 
