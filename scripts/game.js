b88688cf8ebc6863370647c7cabcefc0912dae30
Trevor Mugo <trevormugolawrence@gmail.com>
2019-03-04 07:57:23 +0300

    logic2

--- scripts/game.js
+++ scripts/game.js
@@ -7,19 +7,23 @@ $(document).ready(function(){
   if(checkLvl === "easy")
   {
     //the functionality for easy goes here;
-    alert("this is super easy");
     var name = prompt("What is you're name?");
     document.getElementsByClassName("playername")[0].textContent = name;
     document.getElementsByClassName("levelof")[0].textContent = "  LEVEL : EASY";
+
   }
   else if(checkLvl === "hard")
   {
     //the functionality for hard goes here
-    alert("this is super hard");
+    var name = prompt("What is you're name?");
+    document.getElementsByClassName("playername")[0].textContent = name;
+    document.getElementsByClassName("levelof")[0].textContent = "  LEVEL : DIFFICULT";
   }
   else
   {
     //an error occured
-    alert("an error!! did you choose a difficulty level?");
+    var name = prompt("What is you're name?");
+    document.getElementsByClassName("playername")[0].textContent = name;
+    document.getElementsByClassName("levelof")[0].textContent = "ERROR";
   }
 });
